2508a4a8e2555579c629a57fa8039928c3166bdc
jb <jeanbenoit.delbrouck@gmail.com>
2021-05-10 17:22:37 +0200

    ok

--- vilmedic/datasets/__init__.py
+++ vilmedic/datasets/__init__.py
@@ -1,8 +1,11 @@
 import copy
 from .text_rnn import TextDatasetRNN
 from .multi_rnn import MultiDatasetRNN
-from .vqa_dataset import VQADataset
-from .vqg_dataset import VQGDataset
-from .mimic_captioning_dataset import MimicCaptioningDataset
-from .huggingface.im_text_hug import ImageTextDatasetHug
 from .huggingface.text_hug import TextDatasetHug
+from .huggingface.im_text_hug import ImageTextDatasetHug
+#
+# from .vqa_dataset import VQADataset
+# from .vqg_dataset import VQGDataset
+# from .mimic_captioning_dataset import MimicCaptioningDataset
+# from .huggingface.vqg_dataset_hug import VQGDatasetHug
+# from .huggingface.vqa_dataset_hug import VQADatasetHug
--- vilmedic/networks/huggingface/__init__.py
+++ vilmedic/networks/huggingface/__init__.py
@@ -1,2 +1,4 @@
-from .im2seq import Im2SeqHug
 from .seq2seq import Seq2SeqHug
+# from .vqa2seq import VQA2SeqHug
+# from .visual2seq import Visual2SeqHug
+# from .im2seq import Im2SeqHug

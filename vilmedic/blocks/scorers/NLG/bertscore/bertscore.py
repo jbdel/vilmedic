7fef402d21ff8eb57f0f3b096e42975cd835af41
jb <jeanbenoit.delbrouck@gmail.com>
2022-06-21 15:07:26 -0700

    minor

--- vilmedic/blocks/scorers/NLG/bertscore/bertscore.py
+++ vilmedic/blocks/scorers/NLG/bertscore/bertscore.py
@@ -27,17 +27,21 @@ class BertScore(nn.Module):
             verbose=False,
             batch_size=64,
         )
-        return torch.mean(f).item(), f
+        return torch.mean(f).item(), f.tolist()
 
 
 if __name__ == '__main__':
-    x,y = (BertScore()(
+    x, y = (BertScore()(
         hyps=[
             "nothing to do lol",
+            "nothing to do x",
             'there are moderate bilateral pleural effusions with overlying atelectasis,  underlying consolidation not excluded. mild prominence of the interstitial  markings suggests mild pulmonary edema. the cardiac silhouette is mildly  enlarged. the mediastinal contours are unremarkable. there is no evidence of  pneumothorax.'
         ],
         refs=[
             'heart size is moderately enlarged. the mediastinal and hilar contours are unchanged. there is no pulmonary edema. small left pleural effusion is present. patchy opacities in the lung bases likely reflect atelectasis. no pneumothorax is seen. there are no acute osseous abnormalities.',
+            'heart size is mildly enlarged. the mediastinal and hilar contours are normal. there is mild pulmonary edema. moderate bilateral pleural effusions are present, left greater than right. bibasilar airspace opacities likely reflect atelectasis. no pneumothorax is seen. there are no acute osseous abnormalities.',
             'heart size is mildly enlarged. the mediastinal and hilar contours are normal. there is mild pulmonary edema. moderate bilateral pleural effusions are present, left greater than right. bibasilar airspace opacities likely reflect atelectasis. no pneumothorax is seen. there are no acute osseous abnormalities.'
         ])
     )
+    print(x)
+    print(y)

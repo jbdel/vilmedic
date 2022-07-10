0bb61b70613561010d2b1bd4e8134ecd10e3d691
khaledsaab <ksaab@stanford.edu>
2022-07-09 22:24:41 -0700

    TUH eeg loader

--- vilmedic/datasets/base/papers/stanford_eeg/eeg_dataloader.py
+++ vilmedic/datasets/base/papers/stanford_eeg/eeg_dataloader.py
@@ -54,7 +54,7 @@ class EegTextDataset(Dataset):
         # TODO: deal with train/val/test splits!!
 
     def __len__(self):
-        return len(self.file_names)
+        return len(self.eeg_file_tuple_dict)
 
     def __getitem__(self, idx):
         file_name = self.file_names[idx]
@@ -67,18 +67,103 @@ class EegTextDataset(Dataset):
         return eeg_clip, eeg_text
 
 
-def test_dataloader_and_encoder():
-    stanford_dataset_dir = "/media/4tb_hdd/eeg_data/stanford/stanford_mini"
-    lpch_dataset_dir = "/media/4tb_hdd/eeg_data/lpch/lpch"
-    reports_dict_pth = "/media/nvme_data/eeg_reports/findings_5k_reports_dict.pkl"
+class TuhEegTextDataset(Dataset):
+    """
+    Pytorch dataset that loads both EEG clips and accompanying text report
+    """
 
-    eeg_ds = EegTextDataset(
-        stanford_dataset_dir,
-        lpch_dataset_dir,
+    def __init__(
+        self,
+        raw_dataset_dir,
+        dataset_dir,
         reports_dict_pth,
-        split_type="train",
+        split_type,
         clip_len=12,
-    )
+    ):
+        """
+        Store the filenames of the seizures to use.
+        Args:
+            stanford_dataset_dir/lpch_dataset_dir: (string) directory containing the EEG files for stanford/lpch data
+            reports_dict_pth: (string) path containing the reports dict
+            split_type: (string) whether train, val, or test set
+            clip_len: (int) how long EEG clip input is in seconds
+        """
+
+        # TODO: load text reports
+        # retrieve paths of all edf files in the raw_dataset_dir
+        # self.edf_files = []
+        # for path, subdirs, files in os.walk(raw_dataset_dir):
+        #     for name in files:
+        #         if ".edf" in name:
+        #             self.edf_files.append(os.path.join(path, name))
+
+        self.eeg_file_tuples = compute_tuh_file_tuples(
+            raw_dataset_dir, dataset_dir, split_type, clip_len, clip_len
+        )
+        self.dataset_dir = dataset_dir
+
+        # # create file_tuple dict
+        # eeg_file_tuple_dict = {}
+        # for entry in eeg_file_tuples:
+        #     file_name = entry[0].split("/")[-1].split(".edf")[0]
+        #     # filter eeg_file_tuples to only ones with reports
+        #     # if file_name in self.file_names:
+        #     eeg_file_tuple_dict[file_name] = entry
+        # self.eeg_file_tuple_dict = eeg_file_tuple_dict
+
+        self.clip_len = clip_len
+
+        # TODO: deal with train/val/test splits!!
+
+    def __len__(self):
+        return len(self.eeg_file_tuples)
+
+    def __getitem__(self, idx):
+        # TODO: load text reports
+        # eeg_text = self.reports_dict[file_name]
+
+        edf_fn, clip_idx, split = self.eeg_file_tuples[idx]
+        h5_fn = os.path.join(self.dataset_dir, edf_fn.split(".edf")[0] + ".h5")
+
+        eeg_clip = tuh_eeg_loader(
+            h5_fn, clip_idx, clip_len=self.clip_len, stride=self.clip_len
+        )
+
+        eeg_clip = eeg_clip.swapaxes(0, 1).reshape(19, -1).T
+
+        # return eeg_clip, eeg_text
+        return eeg_clip, None
+
+
+def test_dataloader_and_encoder():
+
+    test_tuh = True
+
+    if test_tuh:
+        raw_dataset_dir = "/media/nvme_data/TUH/v1.5.2/edf"
+        dataset_dir = "/media/nvme_data/siyitang/TUH_eeg_seq_v1.5.2/resampled_signal"
+
+        eeg_ds = TuhEegTextDataset(
+            raw_dataset_dir,
+            dataset_dir,
+            reports_dict_pth="TODO",
+            split_type="train",
+            clip_len=12,
+        )
+
+    else:
+
+        stanford_dataset_dir = "/media/4tb_hdd/eeg_data/stanford/stanford_mini"
+        lpch_dataset_dir = "/media/4tb_hdd/eeg_data/lpch/lpch"
+        reports_dict_pth = "/media/nvme_data/eeg_reports/findings_5k_reports_dict.pkl"
+
+        eeg_ds = EegTextDataset(
+            stanford_dataset_dir,
+            lpch_dataset_dir,
+            reports_dict_pth,
+            split_type="train",
+            clip_len=12,
+        )
 
     eeg_model = DenseInception(data_shape=(2400, 19))
 
@@ -87,6 +172,8 @@ def test_dataloader_and_encoder():
     eeg_clip3, _ = eeg_ds[20]
 
     x = torch.Tensor(np.stack([eeg_clip, eeg_clip2, eeg_clip3]))
+
+    breakpoint()
 
     y = eeg_model(x)
 
--- vilmedic/datasets/base/papers/stanford_eeg/eeg_dataloader_utils.py
+++ vilmedic/datasets/base/papers/stanford_eeg/eeg_dataloader_utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import eeghdf
+import h5py
 import os
 
 STANFORD_INCLUDED_CHANNELS = [
@@ -28,6 +29,149 @@ FREQUENCY = 200
 
 SEIZURE_STRINGS = ["sz", "seizure", "absence", "spasm"]
 FILTER_SZ_STRINGS = ["@sz", "@seizure"]
+
+
+## TUH data utils below
+
+
+def compute_tuh_file_tuples(raw_dataset_dir, dataset_dir, split, clip_len, stride):
+    """
+    Args:
+        dataset_dir (str): location where resampled signals are
+        split (str): whether train, dev, test
+        clip_len(int): length of each clip in the input eeg segments
+        stride (int): how to sample clips from eeg signal
+
+    Returns (file_name, clip_idx, seizure_label) tuples
+            for the given split, clip_len, and stride
+    The clip_idx indicates which clip (i.e. segment of EEG signal with clip_len seconds)
+    The stride determines how to sample clips from the eeg signal
+            (e.g. if stride=clip_len we have no overlapping clips)
+    """
+
+    # retrieve paths of all edf files in the dataset_dir for given split
+    edf_files = []
+    edf_fullfiles = []
+    for path, _, files in os.walk(os.path.join(raw_dataset_dir, split)):
+        for name in files:
+            if ".edf" in name:
+                edf_fullfiles.append(os.path.join(path, name))
+                edf_files.append(name)
+
+    resampled_files = os.listdir(dataset_dir)
+    file_tuples = []
+
+    for h5_fn in resampled_files:
+        edf_fn = h5_fn.split(".h5")[0] + ".edf"
+        if edf_fn not in edf_files:
+            continue
+        edf_fn_full = [file for file in edf_fullfiles if edf_fn in file]
+        if len(edf_fn_full) != 1:
+            print(f"{edf_fn} found {len(edf_fn_full)} times!")
+            print(edf_fn_full)
+
+        edf_fn_full = edf_fn_full[0]
+        seizure_times = get_seizure_times(edf_fn_full.split(".edf")[0])
+
+        h5_fn_full = os.path.join(dataset_dir, h5_fn)
+        with h5py.File(h5_fn_full, "r") as hf:
+            resampled_sig = hf["resampled_signal"][()]
+
+        num_clips = (resampled_sig.shape[-1] - clip_len * FREQUENCY) // (
+            stride * FREQUENCY
+        ) + 1
+
+        for i in range(num_clips):
+            start_window = i * FREQUENCY * stride
+            end_window = np.minimum(
+                start_window + FREQUENCY * clip_len, resampled_sig.shape[-1]
+            )
+
+            is_seizure = 0
+            for t in seizure_times:
+                start_t = int(t[0] * FREQUENCY)
+                end_t = int(t[1] * FREQUENCY)
+                if not ((end_window < start_t) or (start_window > end_t)):
+                    is_seizure = 1
+                    break
+            file_tuples.append((edf_fn, i, is_seizure))
+
+    return file_tuples
+
+
+def get_seizure_times(file_name):
+    """
+    Args:
+        file_name: file name of .edf file etc.
+    Returns:
+        seizure_times: list of times of seizure onset in seconds
+    """
+    tse_file = file_name.split(".edf")[0] + ".tse_bi"
+
+    seizure_times = []
+    with open(tse_file) as f:
+        for line in f.readlines():
+            if "seiz" in line:  # if seizure
+                # seizure start and end time
+                seizure_times.append(
+                    [
+                        float(line.strip().split(" ")[0]),
+                        float(line.strip().split(" ")[1]),
+                    ]
+                )
+    return seizure_times
+
+
+def tuh_eeg_loader(
+    h5_fn,
+    clip_idx,
+    time_step_size=1,
+    clip_len=60,
+    stride=60,
+):
+    """
+    Convert entire EEG sequence into clips of length clip_len
+    Args:
+        channel_names: list of channel names
+        clip_idx: index of current clip/sliding window, int
+        h5_fn: file name of resampled signal h5 file (full path)
+        time_step_size: length of each time step, in seconds, int
+        clip_len: sliding window size or EEG clip length, in seconds, int
+        stride: stride size, by how many seconds the sliding window moves, int
+    Returns:
+        eeg_clip: EEG clip
+    """
+
+    physical_clip_len = int(FREQUENCY * clip_len)
+
+    start_window = clip_idx * FREQUENCY * stride
+
+    with h5py.File(h5_fn, "r") as f:
+        signal_array = f["resampled_signal"][()]
+        resampled_freq = f["resample_freq"][()]
+        assert resampled_freq == FREQUENCY
+
+    # (num_channels, physical_clip_len)
+
+    end_window = np.minimum(signal_array.shape[-1], start_window + physical_clip_len)
+    curr_slc = signal_array[:, start_window:end_window]  # (num_channels, FREQ*clip_len)
+    physical_time_step_size = int(FREQUENCY * time_step_size)
+
+    start_time_step = 0
+    time_steps = []
+    while start_time_step <= curr_slc.shape[1] - physical_time_step_size:
+        end_time_step = start_time_step + physical_time_step_size
+        # (num_channels, physical_time_step_size)
+        curr_time_step = curr_slc[:, start_time_step:end_time_step]
+
+        time_steps.append(curr_time_step)
+        start_time_step = end_time_step
+
+    eeg_clip = np.stack(time_steps, axis=0)
+
+    return eeg_clip
+
+    ## Sanford data utils below
 
 
 def compute_stanford_file_tuples(stanford_dataset_dir, lpch_dataset_dir, splits):

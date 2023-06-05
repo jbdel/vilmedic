bc11a53f2250755f6021c88993d9b19c75cbf5ab
Cara Van Uden <cara.vanuden@gmail.com>
2023-06-05 09:06:19 -0700

    bash scripts

--- test_rl_constrain_gt.sh
+++ test_rl_constrain_gt.sh
@@ -1,4 +1,4 @@
-nll_checkpoint=0.554715_6_736581.pth
+rl_checkpoint=0.617898_11_358234.pth
 concept_type=all_concepts
 python bin/ensemble.py config/RRG/baseline-mimic-force.yml \
     dataset.seq.processing=ifcc_clean_report \
@@ -12,6 +12,12 @@ python bin/ensemble.py config/RRG/baseline-mimic-force.yml \
     dataset.force_seq.file=subset.${concept_type}.tok \
     dataset.force_seq.tokenizer_max_len=128 \
     dataset.image.multi_image=3 \
+    model.proto=RRG_SCST \
+    model.top_k=0 \
+    model.scores_weights='[0.01,0.495,0.495]' \
+    model.scores_args='[{},{reward_level: partial}]' \
+    model.scores=[bertscore,radgraph] \
+    model.use_nll=true \
     model.cnn.backbone=densenet121 \
     model.cnn.visual_embedding_dim=1024 \
     ensemblor.batch_size=1 \
@@ -25,6 +31,6 @@ python bin/ensemble.py config/RRG/baseline-mimic-force.yml \
     ensemblor.look_ahead_step=4 \
     ensemblor.look_ahead_width=2 \
     ensemblor.fusion_t=1 \
-    ensemblor.ckpt=${nll_checkpoint} \
+    ensemblor.ckpt=${rl_checkpoint} \
     ckpt_dir=ckpt \
-    name=nll_findings_${concept_type}_constraints_gt_4_bertscore_128
+    name=rl_findings_${concept_type}_constraints_gt_4_bertscore_128

766ac002d85aab1cb02efcc3e2ddb590cf7dbc1c
jb <jeanbenoit.delbrouck@gmail.com>
2023-12-08 16:17:03 -0800

    logo

--- vilmedic/blocks/classifier/classifier.py
+++ vilmedic/blocks/classifier/classifier.py
@@ -1,16 +1,15 @@
-import torch
-import torch.nn as nn
+from torch import Tensor
+from torch.nn import Sequential, Linear, Dropout, Module
 
-
-class Classifier(nn.Module):
+class Classifier(Module):
     def __init__(self, input_size, num_classes, dropout=0., **kwargs):
         super(Classifier, self).__init__()
-        self.classifier = nn.Sequential(
-            nn.Linear(in_features=input_size, out_features=num_classes)
+        self.classifier = Sequential(
+            Linear(in_features=input_size, out_features=num_classes)
         )
-        self.dropout = nn.Dropout(p=dropout)
+        self.dropout = Dropout(p=dropout)
 
-    def forward(self, input: torch.Tensor):
+    def forward(self, input: Tensor):
         output = self.dropout(input)
         output = self.classifier(output)
         return output
